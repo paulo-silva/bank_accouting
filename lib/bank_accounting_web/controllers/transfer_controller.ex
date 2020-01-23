91a2f12f87cdb9e1a4d711cdfe7ed51b99dfaf8f
Paulo Henrique da Silva <paulo.silva@plataformatec.com.br>
2020-01-23 17:22:28 -0300

    Add transfer creation route.

---
+++ lib/bank_accounting_web/controllers/transfer_controller.ex
@@ -0,0 +1,26 @@
+defmodule BankAccountingWeb.TransferController do
+  use BankAccountingWeb, :controller
+
+  alias BankAccounting.Accounts
+
+  def create(conn, %{
+        "source_account_id" => origin_account_id,
+        "destiny_account_id" => destiny_account_id,
+        "amount" => amount
+      }) do
+    origin_account = Accounts.get_account!(origin_account_id)
+    destiny_account = Accounts.get_account!(destiny_account_id)
+
+    case Accounts.transfer_money(origin_account, destiny_account, amount) do
+      {:ok, :transferred} ->
+        conn
+        |> put_status(:created)
+        |> json(%{status: "transferred"})
+
+      {:error, :not_transferred} ->
+        conn
+        |> put_status(:unprocessable_entity)
+        |> json(%{status: "not_transferred"})
+    end
+  end
+end
