a488d43926736b8406632bbce698fae22180b3c8
Paulo Henrique da Silva <paulo.silva@plataformatec.com.br>
2020-01-24 08:59:26 -0300

    Add User schema
    Add Comeonin and bcrypt_elixir to crypt users passwords.

---
+++ lib/bank_accounting/users/user.ex
@@ -0,0 +1,31 @@
+defmodule BankAccounting.Users.User do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  schema "users" do
+    field :email, :string
+    field :password_hash, :string
+    field :password, :string, virtual: true
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(user, attrs) do
+    user
+    |> cast(attrs, [:email, :password])
+    |> validate_required([:email, :password])
+    |> unique_constraint(:email, downcase: true)
+    |> put_pass_hash()
+  end
+
+  def put_pass_hash(changeset) do
+    case changeset do
+      %Ecto.Changeset{valid?: true, changes: %{password: pass}} ->
+        put_change(changeset, :password_hash, Bcrypt.hash_pwd_salt(pass))
+
+      _ ->
+        changeset
+    end
+  end
+end
