a1e1370a2b1a2d963288bf05c6bb7571f250cc97
Paulo Henrique da Silva <paulo.silva@plataformatec.com.br>
2020-01-24 10:50:33 -0300

    Add Users context
    For now, they store functions to handle authentication, like sign and sign out.

---
+++ lib/bank_accounting/users.ex
@@ -0,0 +1,34 @@
+defmodule BankAccounting.Users do
+  @moduledoc """
+  The Users context.
+  """
+
+  alias BankAccounting.Repo
+  alias BankAccounting.AuthToken
+  alias BankAccounting.Services.Authenticator
+  alias BankAccounting.Users.User
+
+  def sign_in_user(email, password) do
+    case Bcrypt.check_pass(Repo.get_by(User, email: email), password) do
+      {:ok, user} ->
+        token = Authenticator.generate_token(user)
+        Repo.insert(Ecto.build_assoc(user, :auth_tokens, %{token: token}))
+
+      error ->
+        error
+    end
+  end
+
+  def sign_out_user(conn) do
+    case Authenticator.get_auth_token(conn) do
+      {:ok, token} ->
+        case Repo.get_by(AuthToken, %{token: token}) do
+          nil -> {:error, :not_found}
+          auth_token -> Repo.delete(auth_token)
+        end
+
+      error ->
+        error
+    end
+  end
+end
