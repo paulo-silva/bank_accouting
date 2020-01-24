e48295cde801ef69097d51d781a5e6d13b594dd4
Paulo Henrique da Silva <paulo.silva@plataformatec.com.br>
2020-01-24 12:19:54 -0300

    Protect routes with authentication layer.

--- lib/bank_accounting_web/router.ex
+++ lib/bank_accounting_web/router.ex
@@ -12,19 +12,19 @@ defmodule BankAccountingWeb.Router do
   scope "/api", BankAccountingWeb do
     pipe_through :api
 
+    scope "/" do
+      pipe_through :authenticate
+
+      resources "/accounts", AccountController, only: [:index, :show, :create, :update, :delete] do
+        get "/balance", Account.BalanceController, :show
+      end
+
+      resources "/transfers", TransferController, only: [:create]
+    end
+
     scope "/auth" do
       post "/sign_in", AuthController, :create
       delete "/sign_out", AuthController, :delete
     end
-  end
-
-  scope "/", BankAccountingWeb do
-    pipe_through :api
-
-    resources "/accounts", AccountController, only: [:index, :show, :create, :update, :delete] do
-      get "/balance", Account.BalanceController, :show
-    end
-
-    resources "/transfers", TransferController, only: [:create]
   end
 end
--- test/bank_accounting_web/controllers/account/balance_controller_test.exs
+++ test/bank_accounting_web/controllers/account/balance_controller_test.exs
@@ -3,8 +3,21 @@ defmodule BankAccountingWeb.Account.BalanceControllerTest do
 
   alias BankAccounting.Accounts
   alias BankAccounting.Accounts.Account
+  alias BankAccounting.Users
 
-  describe "GET /accounts/:id/balance" do
+  describe "with a logged-in user" do
+    setup %{conn: conn, login_as: email} do
+      {:ok, user} = Users.register_user(%{email: email, password: "secret"})
+      {:ok, auth_token} = Users.sign_in_user(user.email, user.password)
+
+      conn =
+        conn
+        |> put_req_header("authorization", "Bearer #{auth_token.token}")
+
+      {:ok, conn: conn}
+    end
+
+    @tag login_as: "tony.stark@avengers.com"
     test "with valid account returns account balance and transfers made", %{conn: conn} do
       {:ok, %Account{id: origin_account_id}} = Accounts.register_account(%{amount: 100})
       {:ok, %Account{id: destiny_account_id}} = Accounts.register_account(%{amount: 0})
@@ -45,10 +58,18 @@ defmodule BankAccountingWeb.Account.BalanceControllerTest do
              } = json_response(conn, 200)
     end
 
+    @tag login_as: "tony.stark@avengers.com"
     test "with invalid account returns nothing", %{conn: conn} do
       conn = get(conn, Routes.account_balance_path(conn, :show, 1))
 
       assert text_response(conn, 404) == "Account not exist"
     end
+  end
+
+  test "requires user authentication on action", %{conn: conn} do
+    conn = get(conn, Routes.account_balance_path(conn, :show, 1))
+
+    assert conn.status == 401
+    assert conn.halted
   end
 end
