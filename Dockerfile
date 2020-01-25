87b2007462e0c8dd0959dde8fa0fabe355395efc
Paulo Henrique da Silva <paulo.silva@plataformatec.com.br>
2020-01-25 10:50:50 -0300

    Fix docker configuration.

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,8 @@ FROM elixir:1.9.4-slim
 # basic dependencies to make it all work
 RUN apt-get update -qq && apt-get -y --allow-unauthenticated install curl libpq-dev postgresql-client git make erlang-crypto apt-transport-https
 
+RUN apt install -y build-essential
+
 # creating a user for the app
 ARG UID=1000
 RUN useradd -m -u $UID paulo-silva
--- lib/bank_accounting/repo.ex
+++ lib/bank_accounting/repo.ex
@@ -4,4 +4,13 @@ defmodule BankAccounting.Repo do
   use Ecto.Repo,
     otp_app: :bank_accounting,
     adapter: Ecto.Adapters.Postgres
+
+  def init(_, config) do
+    config = config
+      |> Keyword.put(:username, System.get_env("PGUSER", "postgres"))
+      |> Keyword.put(:password, System.get_env("PGPASSWORD", "postgres"))
+      |> Keyword.put(:hostname, System.get_env("PGHOST", "localhost"))
+      |> Keyword.put(:port, System.get_env("PGPORT", "5432") |> String.to_integer)
+    {:ok, config}
+  end
 end
